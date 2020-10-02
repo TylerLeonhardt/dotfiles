6a084882f29f2f690d3c3c8e480255dfd7d75d24
Tyler James Leonhardt <tylerl0706@gmail.com>
2020-10-02 19:01:54 +0000

    try changing the shell

--- install.sh
+++ install.sh
@@ -6,4 +6,8 @@ then
     wget -O - https://raw.githubusercontent.com/PowerShell/PowerShell/master/tools/install-powershell.sh | bash -s
 fi
 
+# Make PowerShell the default shell
+pwshLoc=which pwsh
+sudo chsh -s $pwshLoc
+
 pwsh -nologo -noprofile -command ./install.ps1
