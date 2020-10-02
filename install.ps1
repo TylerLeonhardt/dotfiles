48e29183114b2dd70fa45326580815b83940c0ed
Tyler James Leonhardt <tylerl0706@gmail.com>
2020-10-02 18:00:50 +0000

    hopefully work in Codespaces?

--- install.ps1
+++ install.ps1
@@ -1,4 +1,9 @@
-New-Item -Path ~/.config -ItemType SymbolicLink -Value (Join-Path $PSScriptRoot '.config')
+$configPath = (Join-Path $PSScriptRoot '.config')
+
+# Set up symlinks
+Get-ChildItem -Path $configPath | ForEach-Object {
+    New-Item -Path (Join-Path '~/.config' $_.Name) -ItemType SymbolicLink -Value $_.FullName
+}
 
 Install-Module PSDepend
 Invoke-PSDepend
--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 # If PowerShell isn't installed, install PowerShell
 if ! command -v pwsh &> /dev/null
 then
-    wget -O - https://raw.githubusercontent.com/PowerShell/PowerShell/master/tools/install-powershell.sh | bash -s -preview
+    wget -O - https://raw.githubusercontent.com/PowerShell/PowerShell/master/tools/install-powershell.sh | bash -s
 fi
 
 pwsh -nologo -noprofile -command ./install.ps1
