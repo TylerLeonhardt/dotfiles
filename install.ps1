588b5f5ac7a0080a85df8505f9aa51381248cc5a
Tyler James Leonhardt <tylerl0706@gmail.com>
2020-11-18 03:18:35 +0000

    Revert "move to PSGetV3"
    This reverts commit d38fb95129c8bf0d936d55ba17c2bbf44b5d9deb.

--- install.ps1
+++ install.ps1
@@ -5,5 +5,5 @@ Get-ChildItem -Path $configPath | ForEach-Object {
     New-Item -Path (Join-Path '~/.config' $_.Name) -ItemType SymbolicLink -Value $_.FullName
 }
 
-pwsh -c 'Install-Module -Scope CurrentUser -Force -AllowPrerelease -AllowClobber PowerShellGet'
-Install-PSResource -RequiredResourceFile (Join-Path $PSScriptRoot 'requirements.json')
+Install-Module PSDepend -Force
+Invoke-PSDepend -Force (Join-Path $PSScriptRoot 'requirements.psd1')
