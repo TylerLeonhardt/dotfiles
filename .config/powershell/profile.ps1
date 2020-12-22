01c047734dc5a7aa3cb2c03fc880d31c6330b2ea
Tyler James Leonhardt <tylerl0706@gmail.com>
2020-12-21 20:08:48 -0800

    update profile

--- .config/powershell/profile.ps1
+++ .config/powershell/profile.ps1
@@ -1,38 +1,43 @@
-#region Update PowerShell Daily
+#region UX config
 
-$updateJob = Start-ThreadJob -ScriptBlock {
-    Invoke-Expression "& {$(Invoke-RestMethod aka.ms/install-powershell.ps1)} -Daily"
-}
-
-$eventJob = Register-ObjectEvent -InputObject $updateJob -EventName StateChanged -Action {
-    if($Event.Sender.State -eq [System.Management.Automation.JobState]::Completed) {
-    	Get-EventSubscriber $eventJob.Name | Unregister-Event
-	    Remove-Job $eventJob -ErrorAction SilentlyContinue
-    	Receive-Job $updateJob -Wait -AutoRemoveJob -ErrorAction SilentlyContinue
+$hasOhMyPosh = Import-Module oh-my-posh -MinimumVersion 3.0 -PassThru -ErrorAction SilentlyContinue
+if ($hasOhMyPosh) {
+    $themePath = '~/.config/powershell/PoshThemes/TylerLeonhardt.json'
+    if (Test-Path $themePath) {
+        Set-PoshPrompt -Theme $themePath
+    } else {
+        Set-PoshPrompt -Theme material
     }
 }
 
-#endregion
-
-#region Theme config
-
 if (Get-Module PSReadLine) {
-    Import-Module posh-git
-    Import-Module oh-my-posh
-    $ThemeSettings.MyThemesLocation = "~/.config/powershell/oh-my-posh/Themes"
-    Set-Theme Sorin-NL
-
     Set-PSReadLineKeyHandler -Chord Alt+Enter -Function AddLine
-    Set-PSReadLineOption -ContinuationPrompt "  " -Colors @{ Operator = "`e[95m"; Parameter = "`e[95m" }
+    Set-PSReadLineOption -ContinuationPrompt "  " -PredictionSource History -Colors @{
+        Operator = "`e[95m"
+        Parameter = "`e[95m"
+        InlinePrediction = "`e[36;7;238m"
+    }
+
+    # Searching for commands with up/down arrow is really handy.  The
+    # option "moves to end" is useful if you want the cursor at the end
+    # of the line while cycling through history like it does w/o searching,
+    # without that option, the cursor will remain at the position it was
+    # when you used up arrow, which can be useful if you forget the exact
+    # string you started the search on.
+    Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+    Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+    Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
 }
 
 #endregion
 
 #region Helper functions
 
-# allows idweb to be open from mac
+# Allows idweb to be open from mac
 function idweb {
-    kdestroy --all; kinit --keychain tyleonha@REDMOND.CORP.MICROSOFT.COM; open https://idweb -a Safari.app
+    kdestroy --all
+    kinit --keychain tyleonha@REDMOND.CORP.MICROSOFT.COM
+    open https://idweb -a Safari.app
 }
 
 #endregion
@@ -40,14 +45,18 @@ function idweb {
 #region Argument completers
 
 # dotnet CLI
-Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
-    param($commandName, $wordToComplete, $cursorPosition)
-    dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
-        [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+
+if (Get-Command dotnet -ErrorAction SilentlyContinue) {
+    Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
+        param($commandName, $wordToComplete, $cursorPosition)
+        dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
     }
 }
 
-# dotnet suggest-based CLIs
+# `dotnet suggest`-based CLIs
+
 if (Get-Command dotnet-suggest -ErrorAction SilentlyContinue) {
     $availableToComplete = (dotnet-suggest list) | Out-String
     $availableToCompleteArray = $availableToComplete.Split([Environment]::NewLine, [System.StringSplitOptions]::RemoveEmptyEntries)
@@ -69,29 +78,28 @@ Import-Module Microsoft.PowerShell.UnixCompleters -ErrorAction SilentlyContinue
 
 #endregion
 
+#region Update PowerShell Daily
+
+$updateJob = Start-ThreadJob -ScriptBlock {
+    Invoke-Expression "& {$(Invoke-RestMethod aka.ms/install-powershell.ps1)} -Daily"
+}
+
+$eventJob = Register-ObjectEvent -InputObject $updateJob -EventName StateChanged -Action {
+    if($Event.Sender.State -eq [System.Management.Automation.JobState]::Completed) {
+    	Get-EventSubscriber $eventJob.Name | Unregister-Event
+	    Remove-Job $eventJob -ErrorAction SilentlyContinue
+    	Receive-Job $updateJob -Wait -AutoRemoveJob -ErrorAction SilentlyContinue
+    }
+}
+
+#endregion
+
 #region Hooks
 
 # Set CurrentDirectory when LocationChangedAction is invoked.
 # This allows iTerm2's "Reuse previous session's directory" to work
 $ExecutionContext.SessionState.InvokeCommand.LocationChangedAction += {
-    [Environment]::CurrentDirectory = $pwd.ProviderPath
-}
-
-#endregion
-
-#region Global variables
-
-# For PSZoom
-$global:ZoomApiKey = Get-Secret -Name ZoomApiKey -AsPlainText -ErrorAction SilentlyContinue
-$global:ZoomApiSecret = Get-Secret -Name ZoomApiSecret -AsPlainText -ErrorAction SilentlyContinue
-
-#endregion
-
-#region Start up
-
-if (Test-Path "/Applications/Remove Sophos Endpoint.app") {
-    # Since it's a .app, the best we can do is pop the GUI
-    open "/Applications/Remove Sophos Endpoint.app"
+    [Environment]::CurrentDirectory = $pwd.Path
 }
 
 #endregion
